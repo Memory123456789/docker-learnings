2db83434a486fc2361927550859cac4a2953901d
Leelakrishna Muppandam <leelakrishna591@gmail.com>
2025-07-29 01:40:27 +0530

    Update urls.py

--- py-web-app-django/mysite/myapp/urls.py
+++ py-web-app-django/mysite/myapp/urls.py
@@ -4,10 +4,9 @@ from . import views
 
 urlpatterns = [
         path("", views.welcome, name="welcome"),
-        path('info/', views.show_time_ip),
-path("", views.index, name="index"),
-path('counter/', views.counter_view)
-
-
+        path('info/', views.show_time_ip, name='info'),
+        path("", views.index, name="index"),
+        path('counter/', views.counter_view, name='counter'),
+        path('contact/', views.contact, name='contact')
 ]
 
