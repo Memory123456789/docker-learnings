303d7d4e624fec0c82b20e852db1d08c47db8073
Leelakrishna Muppandam <leelakrishna591@gmail.com>
2025-07-29 23:01:18 +0530

    Update urls.py

--- py-web-app-django/mysite/myapp/urls.py
+++ py-web-app-django/mysite/myapp/urls.py
@@ -1,12 +1,10 @@
-# myapp/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-        path("", views.welcome, name="welcome"),
-        path('info/', views.show_time_ip, name='info'),
-        path("", views.index, name="index"),
-        path('counter/', views.counter_view, name='counter'),
-        path('contact/', views.contact, name='contact')
+    path("", views.home, name="home"),
+    path("info/", views.show_time_ip, name="info"),
+    path("counter/", views.counter_view, name="counter"),
+    path("contact/", views.contact, name="contact"),
 ]
 
